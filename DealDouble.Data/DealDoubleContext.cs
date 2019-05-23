dd75b404e313d663550b9e4d39b6310fbadbc074
kader <akader080@gmail.com>
2019-05-23 06:49:38 +0600

    Add entity  database and migration

---
+++ DealDouble.Data/DealDoubleContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DealDouble.Entities;
+
+namespace DealDouble.Data
+{
+    public class DealDoubleContext : DbContext
+    {
+        public DealDoubleContext() : base("name=DealDoubleConnectionString")
+        {
+        }
+
+        //public DealDoubleContext() : base("DealDoubleConnectionString")
+        //{
+
+        //}
+
+        public DbSet<Auction> MyProperty { get; set; }
+    }
+}
